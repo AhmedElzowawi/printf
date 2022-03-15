fe2446f9956d6728400e2cfdff4f7141e7d1acb9
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-15 21:32:52 +0100

    fixed

--- main.h
+++ main.h
@@ -24,6 +24,8 @@ int print_s(va_list a);
 int print_d(va_list args);
 int print_r(va_list args);
 int print_R(va_list args);
+int print_S(va_list args);
+int print_b(va_list args);
 
 int _printf(const char *format, ...);
 int (*get_op_func(const char *format, int n))(va_list args);
