4d3fac266e5964bcb1be2d47f9d0c1513eb7a72b
Ahmed Elzowawi <ahmed.elzowawi@gmail.com>
2022-03-15 13:28:30 +0100

    random

--- main.h
+++ main.h
@@ -23,6 +23,7 @@ int print_c(va_list args);
 int print_s(va_list a);
 int print_d(va_list args);
 int print_r(va_list args);
+int print_R(va_list args);
 
 int _printf(const char *format, ...);
 int (*get_op_func(const char *format, int n))(va_list args);
